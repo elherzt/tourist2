6539842b26a7ee1fa9525beda16f6bc8428fb297
Jesus Romero <jose.romero@tangosource.com>
2013-09-05 15:43:10 -0500

    calcula camino mas corto

--- tourist.rb
+++ tourist.rb
@@ -10,17 +10,19 @@ class Tourist
             flight[count][2] = Time.parse(flight[count][2])
             flight[count][3] = Time.parse(flight[count][3])
             flight[count][4] = flight[count][4].to_f
-
           end
           count += 1
         end
       end
-      pp flight
-      self.search_sequences(flight)
+      #pp flight
+      Sequences.search_sequences(flight)
     end
-
+  end
+  class Sequences
+    @@departure = "no hay"
+    @@check = "no hay"
     def self.search_sequences(flight)
-      best_time = 24
+      min_time = 24
       best_price = 1000
       total = flight[0][0].to_i
       init = 2
@@ -28,33 +30,79 @@ class Tourist
         block_total = flight[init][0].to_i
         block_begin = init+1
         block_end   = init + block_total
+        count = 0 
+        flight_time = []
         for j in block_begin..block_end
-          from = flight[j][0]
-          to = flight[j][1]
-          #binding.pry
-          if (from == "A" and to == "Z")
-            flight_time  = (flight[j][3]-flight[j][2])/(60*60)
-            flight_price = flight[j][4]
-            if (flight_time < best_time)
-              best_time = flight_price
-            end
-            if (flight_price < best_price)
-              best_price = flight_price
-            end
-            puts "mas corta: "
-            pp flight[j]
-          else
-            
-          end
-          
+          flight_time[count] = flight[j]
+          count += 1
         end
+        flight_time.sort_by!{|h| h[2]}
+        puts "---- ---- ----"
+        pp flight_time
+        #binding.pry
+        search_sequences_time(flight_time)
+        puts "---- ---- ----"
+        puts @@departure  
+        puts @@check  
+        min_time = @@final_time if @@final_time < min_time
         init = j+2
       end
     end
-  end
-  
 
-  class Sequences
+    def self.search_sequences_time(flight)
+      init, count, @end_, to, time = "A", 0, "Z", "ZZ", 0
+      #count = 0
+      #@end_ = "Z"
+      #to = "ZZ"
+      #time = 0
+      @@final_time = 24
+      flight.each do |fly|
+        count += 1
+        if fly[0] == init
+          new_departure = fly[2]
+          time = (fly[3] - fly[2])/3600
+          if fly[1] == @end_
+            chec = fly[3]
+            time = (chec - new_departure)/3600
+            if @@final_time > time
+              @@final_time = time 
+              @@check  = fly[3]
+              @@departure = new_departure
+            end
+
+          else
+            routes_recursive(count,fly[1],time,flight,new_departure)
+            #to = fly[1]
+          end
+        else
+        end
+      end
+      puts @@final_time
+    end
+
+    def self.routes_recursive(inicio, to, time, flight, new_departure)
+      for j in inicio..flight.length-1
+        if flight[j][0] == to
+          if flight[j][1] == @end_
+            #binding.pry
+            chec = flight[j][3]
+            time = (chec - new_departure)/3600
+            #binding.pry
+            if @@final_time > time
+              @@final_time = time 
+              @@check  = flight[j][3]
+              @@departure = new_departure
+            end
+            to = "ZZ"
+            time = 0
+          else
+            routes_recursive(j+1, flight[j][1], time, flight, new_departure)
+            #to = flight[j][1]
+          end
+        end
+      end
+    end
+
     def cheap
     end
 
@@ -62,7 +110,7 @@ class Tourist
     end
   end
 
- 
+
   def initialize(file_name)
     Input.read_file(file_name)
   end
@@ -72,4 +120,4 @@ end
 require 'time'
 require 'pp'
 require 'pry'
-Tourist.new("sample-input.txt")
+Tourist.new("input.txt")
